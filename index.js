342a63d8c03bf3cf5c47ca1ef30467ef0531962a
rossan.pereira <rossan.pereira@intranet.barbosa>
2023-09-21 13:59:31 -0300

    Criação e interação do slide

---
+++ index.js
@@ -0,0 +1,25 @@
+let slide = document.querySelector('.slideshow')
+
+let imagens = [
+    './sunny.jpg',
+    './tripulacao.jpg',
+    './zoro.jpg'
+]
+
+let time = 2000
+let count = 0
+
+function moveSlideShow() {
+    slide.src = imagens[count]
+
+    if ( count < imagens.length - 1 ) {
+        count++
+    } else {
+        count = 0
+    }
+
+    setTimeout("moveSlideShow()", time)
+
+}
+
+window.onload = moveSlideShow

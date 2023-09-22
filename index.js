952fd8953f68323546d0a691fffac5c395e8a976
rossanp <rossan.p23@gmail.com>
2023-09-21 23:21:51 -0300

    Criado as ancoras de prox e ant e suas interações

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let slide = document.querySelector('.slideshow')
+let slide = document.getElementById('slideshowImage')
 
 let imagens = [
     './sunny.jpg',
@@ -8,6 +8,8 @@ let imagens = [
 
 let time = 5000
 let count = 0
+let interval; // Variável para armazenar o intervalo
+    console.log(count);
 
 function moveSlideShow() {
     slide.src = imagens[count]
@@ -17,9 +19,40 @@ function moveSlideShow() {
     } else {
         count = 0
     }
-
-    setTimeout("moveSlideShow()", time)
-
 }
 
-window.onload = moveSlideShow
+// Iniciar o slideshow automaticamente
+interval = setInterval(moveSlideShow, time)
+
+// Manipulador de evento para parar o slideshow ao passar o mouse sobre a imagem
+slide.addEventListener('mouseover', function () {
+    clearInterval(interval) // Interrompe o intervalo
+});
+
+// Manipulador de evento para retomar o slideshow ao tirar o mouse de cima da imagem
+slide.addEventListener('mouseout', function () {
+    interval = setInterval(moveSlideShow, time) // Inicia o intervalo novamente
+});
+
+function previousSlide() {
+    if (count > 0) {
+        count--
+    } else {
+        count = imagens.length - 1
+    }
+    moveSlideShow()
+}
+
+function nextSlide() {
+    if (count < imagens.length - 1) {
+        count++
+    } else {
+        count = 0
+    }
+    moveSlideShow()
+}
+
+// Inicialize o slideshow quando a página for carregada
+window.onload = function () {
+    moveSlideShow(); // Garante que a primeira imagem seja exibida após o carregamento
+}
